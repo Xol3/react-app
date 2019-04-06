07718811c5c604ee05e99fd4f598215cfd1c0ca3
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2019-04-06 14:35:05 -0600

    "properly connected index.html to index.js server"

--- server/index.js
+++ server/index.js
@@ -10,11 +10,11 @@ app.use(bodyParser.json()); // transform an obj into a json
 app.use(bodyParser.urlencoded({extended: false}));
 
 // this will connect to directory public in root, public will have an index.html, to an html file
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, '../public')))
 
 // / refers to the root, in case is unable to connect to the public path will give a hello world meaning server works, is like a test to know server is running
 app.get('/', function(req, res) {
-  res.send('Hello World');
+  res.send('Hello World'); // this appears if not able to connect to html
 });
 
 // sets up the port
